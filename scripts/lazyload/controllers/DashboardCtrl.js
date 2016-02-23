1971c4a4fc3f349b7ba2d91c023947f52c55feee
unknown <francisyanga@foundationfootprint.com>
2016-02-23 22:37:13 +1300

    ui validation vendore registration

--- scripts/lazyload/controllers/DashboardCtrl.js
+++ scripts/lazyload/controllers/DashboardCtrl.js
@@ -1,18 +1,32 @@
 ﻿;(function() {
 
     var app = angular.module("app.ctrls");
-    app.controller("DashboardCtrl", ["$scope", function ($scope) {
+    app.controller("DashboardCtrl", ["$scope", '$location', '$anchorScroll', function ($scope, $location, $anchorScroll) {
+        $scope.registration = {};
         $scope.DailyRates = [];
 
-        $scope.schedule = { day: "Monday", type: "Earlybird", from: 8, to: 9, price:1 };
+        $scope.schedule = { day: "Monday", type: "Custom", from: 8, to: 9 };
 
         $scope.add = function () {
+            if ((/^\s*$/).test($scope.schedule.price) || !$scope.schedule.price) {
+                $scope.ParkingError = "Please input price.";
+                $scope.ShowParkingValidationError = true;
+                return;
+            }
+
+            if (parseInt($scope.schedule.from) >= parseInt($scope.schedule.to)) {
+                $scope.ParkingError = "Invalid time range.";
+                $scope.ShowParkingValidationError = true;
+                return;
+            }
+
+            $scope.ShowParkingValidationError = false;
             var rate = $scope.schedule.price;
             var day = $scope.schedule.day;
             var type = $scope.schedule.type;
             var from = $scope.schedule.from;
             var to = $scope.schedule.to;
-            $scope.DailyRates.push({Rate: rate, Day: day, Type: type, From: from, To: to, Delete: false});
+            $scope.DailyRates.push({ Rate: rate, Day: day, Type: type, From: from, To: to, Delete: false });
         };
 
         $scope.delete = function () {
@@ -20,6 +34,45 @@
                 if ($scope.DailyRates[i].Delete) {
                     $scope.DailyRates.splice(i, 1);
                 }
+            }
+        };
+
+        $scope.submit = function () {
+            $location.hash('ValidationErrorAnchor');
+            if (((/^\s*$/).test($scope.registration.email) || !$scope.registration.email) ||
+                ((/^\s*$/).test($scope.registration.firstname) || !$scope.registration.firstname) ||
+                ((/^\s*$/).test($scope.registration.surname) || !$scope.registration.surname) ||
+                ((/^\s*$/).test($scope.registration.address) || !$scope.registration.address)) {
+                $scope.ShowFormValidationError = true;
+                $scope.ShowSuccess = false;
+                $scope.ValidationError = "Please input all required fields.";
+                $anchorScroll();
+                return;
+            }
+            $scope.ShowFormValidationError = false;
+            $scope.ShowSuccess = true;
+            $location.hash('ValidationErrorAnchor');
+            $anchorScroll();
+        };
+
+        $scope.selectOption = function () {
+            switch ($scope.schedule.type) {
+                case "Earlybird":
+                    $scope.schedule.from = 8;
+                    $scope.schedule.to = 18;
+                    $scope.FromDisabled = true;
+                    $scope.ToDisabled = true;
+                    break;
+                case "Night Rate":
+                    $scope.schedule.from = 18;
+                    $scope.schedule.to = 22;
+                    $scope.FromDisabled = true;
+                    $scope.ToDisabled = true;
+                    break;
+                case "Custom":
+                    $scope.FromDisabled = false;
+                    $scope.ToDisabled = false;
+                    break;
             }
         };
 
