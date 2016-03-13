b842c5e8e6118a6f98db1110379c83bffa6ec2d4
fray <francis.yanga@gmail.com>
2016-03-14 10:42:09 +1300

    parking space detail page

--- scripts/lazyload/controllers/ParkingSpaceDetailsCtrl.js
+++ scripts/lazyload/controllers/ParkingSpaceDetailsCtrl.js
@@ -3,6 +3,13 @@
     var app = angular.module("app.ctrls");
     app.controller("ParkingSpaceDetailsCtrl", ["$scope", '$http', 'ParkingSpaceIdParameter', function ($scope, $http, ParkingSpaceIdParameter) {
 
+        $scope.Address = '';
+        $scope.Vendor = '';
+        $scope.Email = '';
+        $scope.Phone = '';
+
+        $scope.Availability = [];
+
         var parkingSpaceId = ParkingSpaceIdParameter.Get();
         if (parkingSpaceId == null)
         {
@@ -28,6 +35,64 @@
                 var marker = new google.maps.Marker({
                     position: new google.maps.LatLng(currentLat, currentLng),
                     map: map
+                });
+
+                $scope.Address = data.data.Address;
+                $scope.Vendor = data.data.Fullname;
+                $scope.Email = data.data.Email;
+                $scope.Phone = data.data.Phone;
+
+                $.each(data.data.Availability, function (index, item) {
+                    var day;
+                    var from;
+                    var To;
+                    switch (item.Day) {
+                        case 0:
+                            day = "Sun";
+                            break;
+                        case 1:
+                            day = "Mon";
+                            break;
+                        case 2:
+                            day = "Tue";
+                            break;
+                        case 3:
+                            day = "Wed";
+                            break;
+                        case 4:
+                            day = "Thu";
+                            break;
+                        case 5:
+                            day = "Fri";
+                            break;
+                        case 6:
+                            day = "Sat";
+                            break;
+                    }
+                    if (item.From < 12) {
+                        from = item.From + ":00 am";
+                    }
+                    if (item.From == 12) {
+                        from = item.From + ":00 pm";
+                    }
+                    if (item.From > 12) {
+                        var t = item.From - 12;
+                        from = t + ":00 pm";
+                    }
+                    if (item.To < 12) {
+                        to = item.To + ":00 am";
+                    }
+                    if (item.To == 12) {
+                        to = item.To + ":00 pm";
+                    }
+                    if (item.To > 12) {
+                        var t = item.To - 12;
+                        to = t + ":00 pm";
+                    }
+                    $scope.Availability.push({
+                        Day: day, From: from, To: to
+                    });
+
                 });
 
             }, function (error) {
